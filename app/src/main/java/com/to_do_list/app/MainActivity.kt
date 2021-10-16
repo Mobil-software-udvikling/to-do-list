55b1ecd8f3a38b24a80026242de536fb3e6a2091
Nichlas Boraso <nibor19@student.sdu.dk>
2021-10-16 16:40:42 +0200

    Implement custom drawer with burger menu
    A few item are added to menu, but does not do anything.

--- app/src/main/java/com/to_do_list/app/MainActivity.kt
+++ app/src/main/java/com/to_do_list/app/MainActivity.kt
@@ -2,10 +2,39 @@ package com.to_do_list.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer)
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when(item.itemId) {
+            android.R.id.home -> {
+                val drawer : DrawerLayout = findViewById(R.id.drawer)
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
 }
