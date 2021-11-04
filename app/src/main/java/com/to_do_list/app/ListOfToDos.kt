50e00d1dc6b8d8c6d798ee51736919391363d1e4
Peter <Peni419@student.sdu.dk>
2021-11-04 17:59:04 +0100

    Small Change to View of List of To Dos

--- app/src/main/java/com/to_do_list/app/ListOfToDos.kt
+++ app/src/main/java/com/to_do_list/app/ListOfToDos.kt
@@ -2,7 +2,9 @@ package com.to_do_list.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListOfToDos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +12,11 @@ class ListOfToDos : AppCompatActivity() {
         setContentView(R.layout.activity_list_of_to_dos)
 
         // Get the Intent that started this activity and extract the string
-        val listTitle = intent.getStringExtra()
+        //val listTitle = intent.getStringExtra()
 
         // Capture the layout's TextView and set the string as the title of the To Do List
-        val textView = findViewById<TextView>(R.id.titleOfList).apply { text = listTitle }
+        //val textView = findViewById<TextView>(R.id.titleOfList).apply { text = listTitle }
+
+        val mAddFab = findViewById<FloatingActionButton>(R.id.add_fab)
     }
 }
