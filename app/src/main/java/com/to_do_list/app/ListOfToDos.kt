7c201829395adf9b8aaabd6b560bedaecd62906e
Peter <Peni419.student.sdu@mail.dk>
2021-11-05 09:50:20 +0100

    Added Intent to Fab in ListOfToDos
    Fab in ListOfToDos now should go to AddToDo activity

--- app/src/main/java/com/to_do_list/app/ListOfToDos.kt
+++ app/src/main/java/com/to_do_list/app/ListOfToDos.kt
@@ -1,10 +1,12 @@
 package com.to_do_list.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class ListOfToDos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,17 @@ class ListOfToDos : AppCompatActivity() {
         // Capture the layout's TextView and set the string as the title of the To Do List
         //val textView = findViewById<TextView>(R.id.titleOfList).apply { text = listTitle }
 
-        val mAddFab = findViewById<FloatingActionButton>(R.id.add_fab)
+        val mAddFab : FloatingActionButton = findViewById(R.id.add_fab)
+        /*
+        mAddFab.setOnClickListener{
+                view -> Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+            .setAction("Action", null)
+            .show()
+        }
+         */
+        mAddFab.setOnClickListener{
+            val intent = Intent(this, AddToDO::class.java)
+            startActivity(intent)
+        }
     }
 }
