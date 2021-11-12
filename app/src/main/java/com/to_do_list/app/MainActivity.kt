114356e1c3e4df40b9f1602cdf17e5d335fab7cb
Nichlas Boraso <nichlasboraso@gmail.com>
2021-11-12 09:09:26 +0100

    Load data from databse onStart

--- app/src/main/java/com/to_do_list/app/MainActivity.kt
+++ app/src/main/java/com/to_do_list/app/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private  var toDoList: MutableList<ToDoList> = ArrayList()
     private lateinit var toDoListAdapter: ToDoListAdapter
 
-
     private lateinit var toDoListDatabase : TodoListDatabse
+
 
 
     //Listens for added results from other activites
@@ -37,15 +37,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //Only if the result code is ok, we add the list to ToDoLists
         if (it.resultCode == Activity.RESULT_OK) {
             val value = it.data?.getStringExtra("ListName")
-            toDoList.add(ToDoList(0, value!!, ""))
-            toDoListDatabase.ToDoListDao().insert(ToDoList(0, value, ""))
-            Log.i("Databse", toDoListDatabase.ToDoListDao().getAll()[0].listName)
-            toDoListDatabase.toDoDao().insert(ToDo(1, "hello world", 1, "Dottore", 1))
+            //toDoList.add(ToDoList(0, value!!, ""))
+            toDoListDatabase.ToDoListDao().insert(ToDoList(0, value!!, ""))
+            //Rewrite the list with the newly added toDoList
+            loadListsFromDatabase()
         }
     }
 
-    //TODO: Load data from the database when app is started.
-    //TODO: Hold a reference to the list we want to add todos to.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -71,6 +69,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         //Room
         toDoListDatabase = TodoListDatabse.getAppDatabse(this)!!
+
+        //Load saved lists from databse
+        loadListsFromDatabase()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -92,5 +93,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         getResult.launch(intent)
     }
 
-
+    fun loadListsFromDatabase(){
+        //Clear all the data from the list
+        toDoList.clear()
+        //Get all list from databse and add them to the list
+        toDoList.addAll(toDoListDatabase.ToDoListDao().getAll())
+    }
 }
